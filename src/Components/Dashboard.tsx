14b4d3e3152467043a7c273fa3380218b4e574dd
Geovane Santana <geovane.santana@ctw.bmwgroup.com>
2023-05-13 23:07:18 +0100

    update Dashboard page to show hello message

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,7 +1,17 @@
+import { useAuth } from '../contexts/AuthContext'
+
 export default function Dashboard() {
+  const { currentUser } = useAuth()
+  const name = currentUser?.email.split('@')[0]
+
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <div className="p-4 md:p-10 mx-auto max-w-7xl">
+      <h1 className="text-gray-700 text-xl font-medium mb-1 capitalize">
+        Hello {name} <span className="text-2xl">👋</span>
+      </h1>
+      <p className="text-gray-500 text-sm font-normal">
+        A list of users retrieved from a reqres API.
+      </p>
+    </div>
   )
 }
